77c5cc6346753a7df08ba73ab1b656ac46c6c9a3
GPI <pirograg@yandex.ru>
2023-05-10 11:11:25 +0300

    Написал код для подключения к базе и добавил файл для SQL запросов

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+   public static final String NAME_USER = "root";
+    public static final String PASSWORD = "kp0kyc89ufsin";
+    public static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static Connection connection;
+    public static Statement statement;
+
+    static {
+        try{
+            connection = DriverManager.getConnection(URL, NAME_USER, PASSWORD);
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static {
+        try {
+            statement = connection.createStatement();
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
 }
---
+++ src/test/resources/test.sql
@@ -0,0 +1,2 @@
+#insert into users (name, age) values ('Grisha', 22);
+select * from users;
