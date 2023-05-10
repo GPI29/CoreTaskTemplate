af9f01359b76377ff97f63a3f2577894dcd3c2ee
GPI <pirograg@yandex.ru>
2023-05-10 17:01:57 +0300

    Написал код для подключения к базе и добавил файл для SQL запросов

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,31 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util worker = new Util();
+        Statement statement = null;
+
+        try {
+            statement = worker.getConnection().createStatement();
+            String query = "CREATE TABLE test.users" +
+                    "(id INTEGER not null, " + "name VARCHAR(45), " +
+                    "lastName VARCHAR(45), " + "age INTEGER, " + "PRIMARY KEY(id))";
+            statement.executeUpdate(query);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        //ResultSet resultSet = statement.executeQuery(query);
+            //while (resultSet.next()){
+            //    User user = new User();
+            //    user.getId(resultSet.getLong(1));
+            //    user.getAge(resultSet.getByte(2));
+            //    user.getName(resultSet.getString(3));
+            //    System.out.println(user);
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,33 +1,38 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.*;
 
 public class Util {
-   public static final String NAME_USER = "root";
+    public static final String NAME_USER = "root";
     public static final String PASSWORD = "kp0kyc89ufsin";
     public static final String URL = "jdbc:mysql://localhost:3306/mysql";
     public static Connection connection;
-    public static Statement statement;
 
     static {
-        try{
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
             connection = DriverManager.getConnection(URL, NAME_USER, PASSWORD);
-        }catch (SQLException throwables){
-            throwables.printStackTrace();
+
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с базой данных установлено");
+            }
+
+            //connection.close();
+            //if (connection.isClosed()) {
+            //    System.out.println("Соединение с базой данных закрыто");
+            //}
+
+        } catch (SQLException e) {
+            e.printStackTrace();
             throw new RuntimeException();
         }
     }
 
-    static {
-        try {
-            statement = connection.createStatement();
-        }catch (SQLException throwables){
-            throwables.printStackTrace();
-            throw new RuntimeException();
-        }
+    public Connection getConnection() {
+        return connection;
     }
 
 }
